0cf2dcb31b4bf7a858bd06d600d46f685a6f63eb
Geanfranco <geanfranco1212@gmail.com>
2026-02-02 20:21:53 -0400

    Feat: Ya estan listas todas las funcionalidades de cifrados en cifrados.js

--- tarea1/buffect/backend/utils/cifrados.js
+++ tarea1/buffect/backend/utils/cifrados.js
@@ -1,32 +1,34 @@
 import crypto from "crypto";
+import fs from "fs";
 
-class Cifrado {
+export default class Cifrado {
   constructor() {}
 
   cifrarSimetrico(datos) {
     const clave = this.generarClaveSimetrica();
     const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv("aes-256-cbc", clave, iv);
-    let encrypted = cipher.update(datos, "utf8", "hex");
-    encrypted += cipher.final("hex");
+    const pdfBuffer = fs.readFileSync(datos);
+
+    const encrypted = Buffer.concat([cipher.update(pdfBuffer), cipher.final()]);
+    // fs.writeFileSync("archivo.pdf.enc", encrypted); // Guardar el archivo cifrado si es necesario
     return {
-      iv: iv.toString("hex"),
+      iv: iv,
       encryptedData: encrypted,
-      clave: clave.toString("hex"),
+      clave: clave,
     };
   }
 
   descifrarSimetrico(encryptedData, clave, iv) {
-    const decipher = crypto.createDecipheriv(
-      "aes-256-cbc",
-      Buffer.from(clave, "hex"),
-      Buffer.from(iv, "hex")
-    );
+    const decipher = crypto.createDecipheriv("aes-256-cbc", clave, iv);
 
-    let decrypted = decipher.update(Buffer.from(encryptedData, "hex"));
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    let decrypted = Buffer.concat([
+      decipher.update(encryptedData),
+      decipher.final(),
+    ]);
+    //fs.writeFileSync("archivo_descifrado.pdf", decrypted); // Guardar el archivo descifrado
 
-    return decrypted.toString("utf8");
+    return decrypted;
   }
 
   generarClaveSimetrica() {
@@ -42,14 +44,54 @@ class Cifrado {
     const encrypted = crypto.publicEncrypt(publicKey, buffer);
     return encrypted.toString("hex");
   }
+
+  descifrarAsimetrico(encryptedData, privateKey) {
+    const buffer = Buffer.from(encryptedData, "hex");
+    const decrypted = crypto.privateDecrypt(privateKey, buffer);
+    return decrypted.toString("utf8");
+  }
+
+  generarClaveAsimetrica() {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+    });
+    return {
+      publicKey: publicKey.export({ type: "pkcs1", format: "pem" }),
+      privateKey: privateKey.export({ type: "pkcs1", format: "pem" }),
+    };
+  }
 }
 
-let mensaje = "Hola, este es un mensaje secreto.";
-console.log("Sifrado y Descifrado Simetrico:");
-let claseCifer = new cifrado();
-let s = claseCifer.cifrarSimetrico(mensaje);
-console.log("Sifrado:", s);
-console.log(
-  "Decifrado:",
-  claseCifer.descifrarSimetrico(s.encryptedData, s.clave, s.iv)
+/* Se Logro muchachos se logrooooooo
+
+// Ejemplo de uso
+console.log("hash del pdf");
+let cifrados = new Cifrado();
+let hash = cifrados.hashDatos(fs.readFileSync("si.pdf"));
+console.log("Hash del PDF:", hash);
+
+console.log("Cifrado y Descifrado Simetrico:");
+let s = cifrados.cifrarSimetrico("si.pdf");
+console.log("Cifrado:", s);
+let pdfDescifrado = cifrados.descifrarSimetrico(s.encryptedData, s.clave, s.iv);
+console.log("Decifrado:", pdfDescifrado.toString("utf8").slice(0, 100) + "...");
+console.log("Ahora con el cifrado Asimetrico:");
+let clavesAsimetricas = cifrados.generarClaveAsimetrica();
+console.log("Claves Asimetricas:", clavesAsimetricas);
+let cifradoAsimetrico = cifrados.cifradoAsimetrico(
+  s.clave.toString("utf8"),
+  clavesAsimetricas.publicKey
 );
+console.log("Cifrado Asimetrico:", cifradoAsimetrico);
+let descifradoAsimetrico = cifrados.descifrarAsimetrico(
+  cifradoAsimetrico,
+  clavesAsimetricas.privateKey
+);
+
+console.log("Descifrado Asimetrico:", descifradoAsimetrico);
+
+const hashFinal = cifrados.hashDatos(pdfDescifrado);
+
+console.log("¿Archivo íntegro?", hashFinal === hash);
+
+*/
